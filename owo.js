65eee8a0e39a0b5daed2082dd9f1eeb3657d5b3d
puge <mail@puge.org>
2019-10-18 17:33:00 +0800

    save

--- owo.js
+++ owo.js
@@ -17,7 +17,7 @@ module.exports = {
     },
     {
       name: 'viewport',
-      content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no',
+      content: 'width=1200, user-scalable=no',
     },
     {
       name: 'format-detection',
